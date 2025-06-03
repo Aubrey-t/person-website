2293338196562751b175c1aa216e968203b5efa5
aubrey tsambatare <aubreytsambatare@aubreys-MacBook-Pro.local>
2025-06-02 22:18:11 -0400

    Remove unused variables from SideNavigation to fix Vercel build

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -4,9 +4,5 @@ import { useRouter, usePathname } from "next/navigation";
 const tabs = ["/", "/projects", "/experience", "/#education", "/contact"];
 
 export default function SideNavigation() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const currentTab = tabs.indexOf(pathname);
-
   return null;
 } 
