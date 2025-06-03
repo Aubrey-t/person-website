2b0389fa316644d8efa53410df3c89afa1e3eb6b
aubrey tsambatare <aubreytsambatare@aubreys-MacBook-Pro.local>
2025-06-02 22:35:13 -0400

    Remove all unused imports and variables from SideNavigation for clean Vercel deploy

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,7 +1,4 @@
 "use client";
-import { useRouter, usePathname } from "next/navigation";
-
-const tabs = ["/", "/projects", "/experience", "/#education", "/contact"];
 
 export default function SideNavigation() {
   return null;
