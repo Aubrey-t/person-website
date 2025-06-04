75b52e1527f5875b5d1913f27f202bb50f564e15
aubrey tsambatare <aubreytsambatare@aubreys-MacBook-Pro.local>
2025-06-04 13:20:11 -0400

    Add Google Analytics (gtag.js) to all pages via layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import SideNavigation from "@/components/SideNavigation";
 import ClientNavBar from "@/components/ClientNavBar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-black`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V7XF73Q7V5"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V7XF73Q7V5');
+          `}
+        </Script>
         <ClientNavBar />
         <SideNavigation />
         {children}
